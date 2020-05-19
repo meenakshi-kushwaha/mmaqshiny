d98d58a8f437974fa3b5a14b2e666d202a3a10d0
adithirgis <adithi@ilklabs.com>
2020-05-19 21:43:44 +0530

    tests added

--- inst/shiny/tests/test_joined_file.R
+++
@@ -1,6 +0,0 @@
-app <- ShinyDriver$new("../", loadTimeout = 1e+05)
-app$snapshotInit("test_joined_file")
-
-app$snapshot()
-app$setInputs(tabs1 = "3", timeout_ = 10000)
-app$snapshot()
--- inst/shiny/tests/test_plot.R
+++
@@ -1,5 +0,0 @@
-app <- ShinyDriver$new("../", loadTimeout = 1e+05)
-app$snapshotInit("test_plot")
-
-app$setInputs(tabs1 = "2", timeout_=100000)
-app$snapshot()
