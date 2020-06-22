8c1a870cb939499c00cd3b6d8ea03a3d7a4215c3
adithirgis <adithi@ilklabs.com>
2020-06-22 20:46:22 +0530

    removed desc diff

--- R/mmaqshiny_run.R
+++ R/mmaqshiny_run.R
@@ -10,7 +10,7 @@
 #' data collected by specific devices installed on a moving platform.
 #' @keywords mmaqshiny
 #' @examples
-#' if(interactive()){
+#' \dontrun{
 #' library(mmaqshiny)
 #' mmaqshiny::mmaqshiny_run()
 #' }
