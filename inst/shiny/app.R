dc8c5572ce5744ff54fda28821342f3e7fa0255c
adithirgis <adithi@ilklabs.com>
2021-11-17 19:44:38 +0530

    added

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -357,80 +357,81 @@ server <- function(input, output, session) {
                                  as.POSIXct(paste(as.Date(Date, format = "%d-%m-%Y"), Time),
                                             tz = time_z))
         }
-        JSON_csv
+        BC_f <- JSON_csv
+        BC_f$Date <- BC_f$date1 # "%Y/%m/%d", "%d-%m-%Y"
+        ef_file <- BC_f %>%
+          filter(Status == 0) %>%
+          dplyr::select(Date, ATN, BC) %>%
+          mutate(BC1 = (BC / 1000), LD = BC1 - rollapply(BC1, FUN = mean,
+                                                         width = 30, align = "center",
+                                                         partial = TRUE )) %>%
+          mutate(LD75 = runquantile(LD, 300, 0.75, type = 2,
+                                    endrule = c("NA")),
+                 LD25 = runquantile(LD, 300, 0.25, type = 2,
+                                    endrule = c("NA")),
+                 BC2 = BC1, BC3 = BC1) %>%
+          mutate(BC2 = ifelse(BC2 >= 0, 0, BC2),
+                 BC2 = ifelse(BC2 < 0, 1, BC2)) %>%
+          mutate(BC2 = rollapply(BC2, FUN = mean, width = 5,
+                                 align = "center", partial = TRUE),
+                 cev1 = ifelse((LD > 5 * LD75) | (LD < 5 * LD25), BC1, NA))
+        ATN <- ef_file[1, 2]
+        # ef_file$ATN <- ef_file$ATN-(ATN) # for new filter
+        BC_Final <- ef_file
+        CEV <- data.frame(ef_file$Date, ef_file$cev1)
+        CEV$ef_file.cev1[!is.na(CEV$ef_file.cev1)] <- 1
+        CEV <- data.frame(CEV)
+        date_file <- data.frame(ef_file$Date, ef_file$BC2, ef_file$BC3)
+        CEV <- CEV %>%
+          drop_na(ef_file.cev1)
+        setDT(CEV)
+        setDT(date_file)
+        cev_file <- date_file[CEV, on = c("ef_file.Date")]
+        cev_file <- cev_file[!(cev_file$ef_file.BC2 == 0), ]
+        cev_file <- xts(cev_file, order.by = cev_file$ef_file.Date)
+        ef_file <- data.frame(ef_file)
+        CE <- data.frame(index(CEV))
+        i <- index(cev_file)
+        i_old <- index(cev_file)
+        i <- index(cev_file) + 1
+        j <- index(cev_file) + 2
+        k <- index(cev_file) - 2
+        l <- index(cev_file) - 1
+        i <- cbind(as.character(i), as.character(i_old), as.character(j),
+                   as.character(k), as.character(l))
+        Date_cev <- data.frame(i)
+        remove_cev <- data.frame(Date = unlist(Date_cev, use.names = FALSE))
+        Date_Table <- unique(remove_cev[c("Date")])
+        if (nrow(Date_Table) == 0 | is.na(nrow(Date_Table))) {
+          BC <- ef_file %>%
+            mutate(BC_factor = 1)
+        } else {
+          Date_Table <- Date_Table %>%
+            mutate(BC_Factor = 0, Date = as.POSIXct(Date, tz = time_z))
+          setDT(Date_Table)
+          setDT(ef_file)
+          BC <- Date_Table[ef_file, on = c("Date")]
+          BC$BC_Factor[is.na(BC$BC_Factor)] <- 1
+        }
+        BC <- BC %>%
+          mutate(BC_Fi = BC_Factor * BC1)
+        BC$BC_Fi[BC$BC_Fi == 0] <- NA
+        BC <- BC %>%
+          mutate(Tr = exp(-ATN / 100),
+                 CF = (1 / ((0.88 * Tr) + 0.12)),
+                 BC_Final = BC_Fi * CF)
+        BC$BC_Fi[BC$BC_Fi < 0] <- NA
+        BC$BC_Fi[is.na(BC$BC_Fi)] <- " "
+        BC$BC_Final[BC$BC_Final < 0] <- NA
+        BC$BC_Final[is.na(BC$BC_Final)] <- " "
+        BC_f <- BC %>%
+          dplyr::select("date" = Date, "BC" = BC3, "BC_NR" = BC_Fi, "BC_NR_LC" = BC_Final) %>%
+          mutate_at(c("BC", "BC_NR", "BC_NR_LC"), as.numeric) %>%
+          arrange(date)
+        BC_f <- as.data.frame(BC_f)
       })
       BC_f <- do.call(rbind, df_list)
-      BC_f$Date <- BC_f$date1 # "%Y/%m/%d", "%d-%m-%Y"
-      ef_file <- BC_f %>%
-        filter(Status == 0) %>%
-        dplyr::select(Date, ATN, BC) %>%
-        mutate(BC1 = (BC / 1000), LD = BC1 - rollapply(BC1, FUN = mean,
-                                                       width = 30, align = "center",
-                                                       partial = TRUE )) %>%
-        mutate(LD75 = runquantile(LD, 300, 0.75, type = 2,
-                                  endrule = c("NA")),
-               LD25 = runquantile(LD, 300, 0.25, type = 2,
-                                  endrule = c("NA")),
-               BC2 = BC1, BC3 = BC1) %>%
-        mutate(BC2 = ifelse(BC2 >= 0, 0, BC2),
-               BC2 = ifelse(BC2 < 0, 1, BC2)) %>%
-        mutate(BC2 = rollapply(BC2, FUN = mean, width = 5,
-                               align = "center", partial = TRUE),
-               cev1 = ifelse((LD > 5 * LD75) | (LD < 5 * LD25), BC1, NA))
-      ATN <- ef_file[1, 2]
-      # ef_file$ATN <- ef_file$ATN-(ATN) # for new filter
-      BC_Final <- ef_file
-      CEV <- data.frame(ef_file$Date, ef_file$cev1)
-      CEV$ef_file.cev1[!is.na(CEV$ef_file.cev1)] <- 1
-      CEV <- data.frame(CEV)
-      date_file <- data.frame(ef_file$Date, ef_file$BC2, ef_file$BC3)
-      CEV <- CEV %>%
-        drop_na(ef_file.cev1)
-      setDT(CEV)
-      setDT(date_file)
-      cev_file <- date_file[CEV, on = c("ef_file.Date")]
-      cev_file <- cev_file[!(cev_file$ef_file.BC2 == 0), ]
-      cev_file <- xts(cev_file, order.by = cev_file$ef_file.Date)
-      ef_file <- data.frame(ef_file)
-      CE <- data.frame(index(CEV))
-      i <- index(cev_file)
-      i_old <- index(cev_file)
-      i <- index(cev_file) + 1
-      j <- index(cev_file) + 2
-      k <- index(cev_file) - 2
-      l <- index(cev_file) - 1
-      i <- cbind(as.character(i), as.character(i_old), as.character(j),
-                 as.character(k), as.character(l))
-      Date_cev <- data.frame(i)
-      remove_cev <- data.frame(Date = unlist(Date_cev, use.names = FALSE))
-      Date_Table <- unique(remove_cev[c("Date")])
-      if (nrow(Date_Table) == 0 | is.na(nrow(Date_Table))) {
-        BC <- ef_file %>%
-          mutate(BC_factor = 1)
-      } else {
-        Date_Table <- Date_Table %>%
-          mutate(BC_Factor = 0, Date = as.POSIXct(Date, tz = time_z))
-        setDT(Date_Table)
-        setDT(ef_file)
-        BC <- Date_Table[ef_file, on = c("Date")]
-        BC$BC_Factor[is.na(BC$BC_Factor)] <- 1
-      }
-      BC <- BC %>%
-        mutate(BC_Fi = BC_Factor * BC1)
-      BC$BC_Fi[BC$BC_Fi == 0] <- NA
-      BC <- BC %>%
-        mutate(Tr = exp(-ATN / 100),
-               CF = (1 / (0.88 * Tr + 0.12)),
-               BC_Final = BC_Fi * CF)
-      BC$BC_Fi[BC$BC_Fi < 0] <- NA
-      BC$BC_Fi[is.na(BC$BC_Fi)] <- " "
-      BC$BC_Final[BC$BC_Final < 0] <- NA
-      BC$BC_Final[is.na(BC$BC_Final)] <- " "
-      BC_f <- BC %>%
-        dplyr::select("date" = Date, "BC" = BC3, "BC_NR" = BC_Fi, "BC_NR_LC" = BC_Final) %>%
-        mutate_at(c("BC", "BC_NR", "BC_NR_LC"), as.numeric)
-      BC_f <- as.data.frame(BC_f)
-      BC_date <- as.Date(BC_f[1, "date"], format = "%y-%m-%d", tz = time_z)
+      BC_date <- as.Date(BC_f[1, "date"], format = "%Y-%m-%d", tz = time_z)
       files3 <- lapply(path, function(y) {
         JSON_csv_header <- read.delim(y, header = FALSE, sep = ",", skip = 15,
                                       row.names = NULL, stringsAsFactors = FALSE)
